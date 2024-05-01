653a913e2aad7d5572497acb5d43ecef7b30e4cf
sophie9260 <158618434+sophie9260@users.noreply.github.com>
2024-05-01 13:24:56 -0700

    completed

--- task1.py
+++ task1.py
@@ -1,6 +1,17 @@
 def A():
+    print("Hello")
 
 def B():
-
+    print("How are you")
 
 def C():
+    print("Hi")
+
+n = input("Choose a letter between: A, B or C => ")
+
+if n == "A":
+    A()
+elif n == "B":
+    B()
+elif n == "C":
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,21 @@ displays instructions and how to play
 game()
 plays the games
 """
+
+def title():
+    print("Guessing game!")
+    print("Here are the instructions on how to play: please guess a number from 1 to 5, keep guessing until you get it right.")
+
+def game():
+    n = ""
+    while n != 4:
+        n =  input("Guess a number from 1 to 5 => ")
+        n = float(n)
+        if n != 4:
+            print("Wrong guess... try again!")
+            print("===     ===")
+        else:
+            print("Good job you guessed it!")
+
+title()
+game()
